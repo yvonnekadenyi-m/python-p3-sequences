52f5dfa3212da2530d93293b0ecc0fbe7df27159
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-11-25 19:57:04 +0300

    added to sequences.py

--- lib/sequences.py
+++ lib/sequences.py
@@ -4,4 +4,13 @@ def print_fibonacci(length):
     if length <= 0:
         print([])
         return
-    
+    #!/usr/bin/env python3
+
+def print_fibonacci(length):
+    if length <= 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+
