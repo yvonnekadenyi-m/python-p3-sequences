203bd595eee62d8b9d1819c59e510264ae92fb86
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-11-25 19:59:08 +0300

    added to sequences.py

--- lib/sequences.py
+++ lib/sequences.py
@@ -13,4 +13,17 @@ def print_fibonacci(length):
     elif length == 1:
         print([0])
         return
+#!/usr/bin/env python3
+
+def print_fibonacci(length):
+    if length <= 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < length:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2]
 
